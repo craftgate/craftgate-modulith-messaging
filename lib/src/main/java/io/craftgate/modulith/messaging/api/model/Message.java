61181265d51ff46a4c86762b87bf82299b396c0f
Lemi Orhan Ergin <lemi@craftgate.io>
2023-02-04 23:46:31 +0300

    Add javadoc

--- lib/src/main/java/io/craftgate/modulith/messaging/api/model/Message.java
+++ lib/src/main/java/io/craftgate/modulith/messaging/api/model/Message.java
@@ -14,18 +14,41 @@ import java.util.UUID;
 @SuperBuilder
 public abstract class Message {
 
+    /**
+     * The "messageCreatedAt" field shows the creation date of the message. It is the current date time in the system's
+     * timezone.
+     */
     @Builder.Default
     private LocalDateTime messageCreatedAt = CurrentDateTimeManager.now();
 
+    /**
+     * The "traceId" field is a unique value for each created message. It can be used for troubleshooting.
+     */
     @Builder.Default
     private String traceId = UUID.randomUUID().toString();
 
+    /**
+     * The "producer" field shows the producer of this message. Leave it empty if it is not important. For instance,
+     * if you may want to set it to something in order to generate the name of kafka topic.
+     */
     @Setter
     private String producer;
 
+    /**
+     * The "key" field is a unique identifier for messages. It is mainly used for marking one or multiple messages
+     * with same value. For instance, if you want to process multiple messages at the same partition of kafka (i.e. in
+     * the same queue), give same key to all these messages and kafka will process them in order.
+     */
     @Setter
     private String key;
 
+    /**
+     * The "type" field is the given type stated in MessageType annotation as in the example blow:
+     * <pre>@MessageType(name = "USER_CREATED")</pre>
+     * If annotation is missing, the class name of current message class is returned.
+     *
+     * @return String
+     */
     @ToString.Include(name = "type")
     public String getType() {
         MessageType messageType = this.getClass().getAnnotation(MessageType.class);
